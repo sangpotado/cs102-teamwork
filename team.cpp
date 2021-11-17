952774a7c8788a5a42f655aed15406d68e81d904
sangpotado <75396243+sangpotado@users.noreply.github.com>
2021-11-16 20:56:41 -0800

    Add files via upload

---
+++ team.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct team {
+    string name;
+    int score;
+};
+
+
+int main(){
+    fstream newfile;
+    newfile.open("SuperBowl.txt",ios::in); //open a file to perform read operation using file object
+    if (newfile.is_open()){ //checking whether the file is open
+        string tp;
+        while(getline(newfile, tp)){ //read data from file object and put it into string.
+            cout << tp << "\n"; //print the data of the string
+        }
+        newfile.close(); //close the file object.
+    }
+}
